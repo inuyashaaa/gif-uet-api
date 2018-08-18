8d8938fe7bb69733e5d9d6d4b64b2e8c9de18240
inuyashaaa <huymanhtmhp@gmail.com>
2018-08-18 16:32:18 +0700

    Add send message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const fs = require("fs")
+const path = require('path')
 
 var Koa = require('koa')
 var Router = require('koa-router')
 const login = require("facebook-chat-api")
+const koaBody = require('koa-body')
 
 var app = new Koa();
 var router = new Router();
@@ -10,25 +12,29 @@ var router = new Router();
 router.get('/', (ctx, next) => {
   ctx.body = "Welcome to Gifur <3"
 })
-router.get('/login', (ctx, next) => {
-    login({email: "FB_EMAIL", password: "FB_PASSWORD"}, (err, api) => {
-        if(err) return console.error(err);
-    
-        fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
-    });
-})
-router.get('/send-message', (ctx, next) => {
-  ctx.body = "Welcome to Gifur <3"
-  login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
-    if(err) return console.error(err)
 
-    var manhID = "100004030405392"
-    var msg = {
-      attachment: fs.createReadStream(__dirname + '../manh2.png')
-      // url: "https://i.imgur.com/z7TcWyP.gif"
+router.post('/login', koaBody(), (ctx, next) => {
+  const { email, password } = ctx.request.body
+  login({email, password}, (err, api) => {
+    if(err) return console.log(err)
+
+    const userId = api.getCurrentUserID()
+    fs.writeFileSync(`${userId}.json`, JSON.stringify(api.getAppState()));
+  });
+  ctx.body = 'Login'
+})
+
+router.post('/send-message', koaBody(), (ctx, next) => {
+  const { userSendId, userRecievedId, imageName } = ctx.request.body
+  login({appState: JSON.parse(fs.readFileSync(`${userSendId}.json`, 'utf8'))}, (err, api) => {
+    if(err) return console.error(err)
+    const manhID = "100004030405392"
+    const message = {
+      attachment: fs.createReadStream(path.resolve(__dirname, `../public/images/${imageName}`))
     }
-    api.sendMessage(msg, manhID)
+    api.sendMessage(message, userRecievedId)
   })
+  ctx.body = 'Message'
 })
 
 app
